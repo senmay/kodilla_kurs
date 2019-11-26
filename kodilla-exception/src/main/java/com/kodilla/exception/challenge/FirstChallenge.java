79f532e068e594593aa21d46f8fbfbe0d2de3e80
senmay <dominik.szarzynski@gmail.com>
2019-11-26 21:54:21 +0100

    m8.z1

--- kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
@@ -1,4 +1,26 @@
 package com.kodilla.exception.challenge;
 
 public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            FirstChallenge firstChallenge = new FirstChallenge();
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+
+        } catch(ArithmeticException e) {
+            System.out.println("Can't divide by 0 " + e);
+
+        } finally {
+            System.out.println("The end.");
+        }
+    }
 }
