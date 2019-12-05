bc56c78238fb03273e629cf809ece3be545d91b8
senmay <dominik.szarzynski@gmail.com>
2019-12-05 22:38:24 +0100

    ostatnie

--- kodilla-exception/src/main/java/com/kodilla/exception/test/Main.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Main.java
@@ -1,4 +1,15 @@
 package com.kodilla.exception.test;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Flight flight = new Flight("Krakow","Okecie");
+        SrFlight flightSearch = new SrFlight();
+
+        try{
+            flightSearch.findFilght(flight);
+        } catch(RouteNotFoundException e) {
+            System.out.println(e);
+        }
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SrFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SrFlight.java
@@ -1,4 +1,23 @@
 package com.kodilla.exception.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SrFlight {
+
+    public void findFilght(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> airport = new HashMap<>();
+        airport.put("Rio de Janerio", true);
+        airport.put("Krakow", true);
+        airport.put("Croatia", true);
+        airport.put("Germany", true);
+        airport.put("Wlochy", true);
+
+        if(airport.containsKey(flight.getArrivalAirport())) {
+            System.out.println("Arrival possible: " + flight.getArrivalAirport());
+        }
+        else
+            throw new RouteNotFoundException("No airport");
+
+    }
 }
