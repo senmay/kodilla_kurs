68ac81d502e489991b5da1e3dd49cf0a186e87df
senmay <dominik.szarzynski@gmail.com>
2020-02-15 21:05:59 +0100

    9.2 blad przy testach

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -0,0 +1,39 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Calculator {
+
+    private Display display;
+
+    public Calculator(Display display) {
+        this.display = display;
+    }
+
+    public double add (double a, double b) {
+        double result = a+b;
+        display.displayValue(result);
+        return result;
+    }
+
+    public double subtract (double a, double b) {
+        double result = a-b;
+        display.displayValue(result);
+        return result;
+    }
+
+    public double divide (double a, double b) {
+        double result = a/b;
+        display.displayValue(result);
+        return result;
+    }
+
+    public double multiply (double a, double b) {
+        double result = a * b;
+        display.displayValue(result);
+        return result;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodila/spring/calculator/CalcTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodila.spring.calculator;
+
+import com.kodilla.spring.calculator.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+
+@SpringBootTest (classes = Calculator.class)
+public class CalcTestSuite {
+    @Test
+    public void testCalculations() {
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //when
+        double add = calculator.add(2, 2);
+        double sub = calculator.subtract(10, 3);
+        double mul = calculator.multiply(5, 10);
+        double div = calculator.divide(12, 4);
+
+        //then
+        Assert.assertEquals(4, add, 0);
+        Assert.assertEquals(7, sub, 0);
+        Assert.assertEquals(50, mul, 0);
+        Assert.assertEquals(3, div, 0);
+    }
+}
